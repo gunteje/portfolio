1d2ddde80a72423f53135503f418a5dd6b57aa83
kgw <kgw982653@gmail.com>
2025-12-30 16:31:41 +0900

    배포수정 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 저장소 이름이 'portportfolio'이므로 base를 수정
-  base: '/portportfolio/',
+  // 저장소 이름이 'portfolio'이므로 base를 수정
+  base: '/portfolio/',
 })
